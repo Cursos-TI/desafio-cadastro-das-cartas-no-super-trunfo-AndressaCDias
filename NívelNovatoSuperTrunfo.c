b3985f667c2396abc5d4451469e6e7c11dcb1b42
AndressaCDias <138800798+AndressaCDias@users.noreply.github.com>
2025-03-12 17:54:23 +0000

    Criei o printf para exibir o nome do jogo

---
+++ NívelNovatoSuperTrunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main(){;
+    printf("Desafio Super Trunfo \n");
+
+    int população;
+    float area;
+    float pib; 
+    int numero_populacao;
+
+
+
+
+
+
+
+        return 0;
+    }
